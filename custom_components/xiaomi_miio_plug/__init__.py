14c647e4930c241a9a628e2c9bf3e8bddff08522
tsunglung <tsunglung@github.com>
2026-01-20 17:24:16 +0800

    Fix deprecated config entry handling in OptionsFlow

--- custom_components/xiaomi_miio_plug/__init__.py
+++ custom_components/xiaomi_miio_plug/__init__.py
@@ -126,8 +126,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     hass.data[DOMAIN][host] = plug
     # init setup for each supported domains
-    for platform in DOMAINS:
-        hass.async_create_task(hass.config_entries.async_forward_entry_setup(
-            entry, platform))
+    await hass.config_entries.async_forward_entry_setups(entry, DOMAINS)
 
     return True
